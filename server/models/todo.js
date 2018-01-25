7501b89aead78d10e8fd21f49691f84711fa2d40
Francis Leong <francisleong2@gmail.com>
2018-01-24 23:19:33 -0500

    Add POST /todos route and refactor mongoose

---
+++ server/models/todo.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const Todo = mongoose.model('Todo', {
+  text: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  },
+  completedAt: {
+    type: Number,
+    default: null
+  }
+});
+
+module.exports = {
+  Todo
+};
+
+// const newTodo = new Todo({
+//   text: 'Cook dinner'
+// });
+
+// newTodo.save().then((doc) => {
+//   console.log('Saved todo', doc);
+// }, (e) => {
+//   console.log('Unable to save todo');
+// });
---
+++ server/models/user.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const User = mongoose.model('User', {
+  email: {
+    type: String,
+    trim: true,
+    required: true,
+    minlength: 1
+  }
+});
+
+module.exports = {
+  User
+};
