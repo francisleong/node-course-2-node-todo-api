91b7ffa80c9df49af278d2cae4202b899adee2dc
Francis Leong <francisleong2@gmail.com>
2018-04-02 20:28:40 -0400

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -97,9 +97,10 @@ app.patch('/todos/:id', (req, res) => {
     res.status(400).send();
   })
 });
+
 // POST /users
 app.post('/users', (req, res) => {
-  const body = _.pick(req.body, ['email', 'password', ])
+  const body = _.pick(req.body, ['email', 'password'])
   const user = new User(body);
 
   user.save().then(() => {
@@ -110,6 +111,19 @@ app.post('/users', (req, res) => {
   }).catch((e) => {
     res.status(400).send(e);
   })
+});
+
+// POST /users/login {email, password}
+app.post('/users/login', (req, res) => {
+  const body = _.pick(req.body, ['email', 'password']);
+  
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) => {
+    res.status(400).send();
+  });
 });
 
 app.get('/users/me/', authenticate, (req, res) => {
